78e376666f6e7c23771ac4f2f1d1e4f6e194afc8
Limon <victoriacuomo11@gmail.com>
2025-07-13 23:45:53 -0300

    guide 5 ej6 frase de conjuntos

--- guide5/6-eliminar-palabras-repetidas.py
+++ guide5/6-eliminar-palabras-repetidas.py
@@ -5,3 +5,20 @@ ordenadas según su longitud. Los signos de puntuación no deben afectar el
 proceso.
 '''
 
+frase = input("Ingrese una frase: ")
+
+# Definir los signos de puntuación manualmente
+puntuacion = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+
+# Eliminar signos de puntuación
+frase_sin_puntuacion = ''.join(c for c in frase if c not in puntuacion)
+
+# Separar en palabras y eliminar repetidas usando un conjunto
+palabras = set(frase_sin_puntuacion.lower().split())
+
+# Ordenar las palabras por longitud
+palabras_ordenadas = sorted(palabras, key=len)
+
+print("Palabras ordenadas por longitud:")
+for palabra in palabras_ordenadas:
+    print(palabra)
